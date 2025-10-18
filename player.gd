00f4fc948bab62901523a10ae610f759b0202901
Sandvitch007 <lukyhuty@gmail.com>
2025-10-18 22:04:02 +0200

    Movement + animacie

--- player.gd
+++ player.gd
@@ -4,11 +4,11 @@ var max_speed = 100
 var last_direction := Vector2(1,0)
 
 func _physics_process(delta):
-	var direction = Input.get_vector("Move_left", "Move_right", "Move_up", "Move_down")
+	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * max_speed
 	move_and_slide()
 	
-	if direction.lenght() > 0:
+	if direction.length() > 0:
 		last_direction = direction
 		play_walk_animation(direction)
 		
